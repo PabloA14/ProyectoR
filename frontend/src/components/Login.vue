48462f1b12a9cb3b9771fa4c093f4a8f84c89ac0
JuDaF-15 <juandda1505@gmail.com>
2023-08-23 17:03:45 -0500

    mod login

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,56 +1,52 @@
 <template>
     <div>
-        <div class="row" style="margin-top: 110px;">
-            <div class="col-4"></div>
-            <div class="col-4">
-                <q-card white bordered class="my-card" style="width: 430px;">
-                    <q-card-section>
-                        <div class="row">
-                            <div class="col-3">
-                                <img id="img"
-                                    src='https://senasofiapluss.com/wp-content/uploads/2022/10/logo-del-sena-verde-1024x1024.jpg'>
-                            </div>
-                            <div class="col-9">
-                                <div id="titulo" class="text-h6" style="text-align: center;">Login</div>
-                            </div>
+        <div class="container">
+            <q-card white bordered class="my-card" style="width: 430px">
+                <q-img :src="url" :ratio="16 / 8" />
+                <q-card-section>
+                    <div class="row">
+                        <div class="col-3">
+                            <img id="img"
+                                src='https://senasofiapluss.com/wp-content/uploads/2022/10/logo-del-sena-verde-1024x1024.jpg'>
                         </div>
-                    </q-card-section>
-                    <q-separator inset class="text-black" />
-                    <q-card-section>
-                        <q-form @submit="onSubmit" class="q-gutter-md">
+                        <div class="col-9">
+                            <div id="titulo" class="text-h6" style="text-align: center;">Login</div>
+                        </div>
+                    </div>
+                </q-card-section>
+                <q-separator inset class="text-black" />
+                <q-card-section>
+                    <q-form @submit="onSubmit" class="q-gutter-md">
 
-                            <q-input color="positive" filled v-model="form.email" label="Correo Electrónico" type="email"
-                                lazy-rules :rules="[val => val && val.length > 0 || 'Este campo es obligatorio']">
-                                <template v-slot:prepend>
-                                    <q-icon name="person" />
-                                </template>
-                            </q-input>
+                        <q-input color="positive" filled v-model="form.email" label="Correo Electrónico" type="email"
+                            lazy-rules :rules="[val => val && val.length > 0 || 'Este campo es obligatorio']">
+                            <template v-slot:prepend>
+                                <q-icon name="person" />
+                            </template>
+                        </q-input>
 
-                            <q-input color="positive" filled type="password" v-model="form.password1" label="Contraseña"
-                                lazy-rules :rules="[
-                                    val => val !== null && val !== '' || 'Este campo es obligatorio',
+                        <q-input color="positive" filled type="password" v-model="form.password1" label="Contraseña"
+                            lazy-rules :rules="[
+                                val => val !== null && val !== '' || 'Este campo es obligatorio',
 
-                                ]">
-                                <template v-slot:prepend>
-                                    <q-icon name="lock" />
-                                </template>
-                            </q-input>
+                            ]">
+                            <template v-slot:prepend>
+                                <q-icon name="lock" />
+                            </template>
+                        </q-input>
 
-                            <div class="row">
-                                <router-link class="full-width" to="/header">
-                                    <q-btn style="color: white;" color="positive" class="full-width" label="Ingresar"
-                                        type="submit" />
+                        <div class="row">
+                            <router-link class="full-width" to="/header">
+                                <q-btn style="color: white;" color="positive" class="full-width" label="Ingresar"
+                                    type="submit" />
 
-                                </router-link>
-                                <!-- type="submit"  -->
-                                <!-- <q-btn style="background-color: green; color: white;" class="full-width" label="Ingresar" /> -->
-                            </div>
-                        </q-form>
-                    </q-card-section>
-                </q-card>
-
-            </div>
-            <div class="col-4"></div>
+                            </router-link>
+                            <!-- type="submit"  -->
+                            <!-- <q-btn style="background-color: green; color: white;" class="full-width" label="Ingresar" /> -->
+                        </div>
+                    </q-form>
+                </q-card-section>
+            </q-card>
         </div>
     </div>
 </template>
@@ -60,7 +56,7 @@ import { ref } from 'vue';
 const email = ref('');
 const password1 = ref('');
 const password2 = ref('');
-
+const url = 'https://static.vecteezy.com/system/resources/previews/002/744/896/non_2x/file-management-illustration-vector.jpg'
 
 const form = ref({
     email: '',
@@ -86,6 +82,14 @@ function onSubmit() {
     width: 80px;
 }
 
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    padding: 20px;
+}
+
 .text-black {
     color: black;
 }
@@ -93,6 +97,5 @@ function onSubmit() {
 #titulo {
     margin-top: 26px;
     font-size: 30px;
-    font-family: 'Times New Roman', Times, serif;
 }
 </style>
