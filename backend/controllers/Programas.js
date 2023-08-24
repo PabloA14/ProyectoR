f351928bfb0c9e20a83f1d6fabfb1fc97e22e378
veronicvega <ardilavegaveronica@gmail.com>
2023-08-24 12:57:53 -0500

    prueba

---
+++ backend/controllers/Programas.js
@@ -0,0 +1,5 @@
+import programasFormacion from "../models/programasFormacion";
+
+const httpProgramas ={
+    
+}
--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,19 +1,4 @@
-// import Login from "../components/Login.vue"
-// import Header from "../components/Header.vue"
-// import Opciones from "../components/Opciones.vue"
-// import instructores from "../components/instructores.vue"
 
-// export const routes = [
-//     { path: "/", component: Login },
-//     {
-//         path: "/header", component: Header, children: [
-//             { path : "/opciones" ,  component : Opciones},
-//             { path : "/Instructores" , component : instructores},
-
-
-//         ]
-//     },
-// ]
 
 import Login from "../components/Login.vue"
 import Header from "../components/Header.vue"
