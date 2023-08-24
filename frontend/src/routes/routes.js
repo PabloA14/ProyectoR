339c2e96003c6235446a15fe949e0393179bdcfc
JuDaF-15 <juandda1505@gmail.com>
2023-08-24 12:35:05 -0500

    modificado

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,11 +1,12 @@
 import Login from "../components/Login.vue"
 import Header from "../components/Header.vue"
+import InicioAdmin from "../components/InicioAdmin.vue"
 
 export const routes = [
     { path: "/", component: Login },
     {
         path: "/header", component: Header, children: [
-            //{ path: "/homeAdmin", component: HomeAdmin },
+            { path: "/inicioAdmin", component: InicioAdmin },
         ]
     },
 ]
