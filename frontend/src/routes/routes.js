7c17b6a7b167d778be7fc6ad2963b2f052039289
JuDaF-15 <juandda1505@gmail.com>
2023-08-24 12:39:05 -0500

    mod

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,43 +1,18 @@
-// import Login from "../components/Login.vue"
-// import Header from "../components/Header.vue"
-// import Opciones from "../components/Opciones.vue"
-// import instructores from "../components/instructores.vue"
-
-// export const routes = [
-//     { path: "/", component: Login },
-//     {
-//         path: "/header", component: Header, children: [
-//             { path : "/opciones" ,  component : Opciones},
-//             { path : "/Instructores" , component : instructores},
-
-
-//         ]
-//     },
-// ]
-
 import Login from "../components/Login.vue"
 import Header from "../components/Header.vue"
-<<<<<<< HEAD
-import InicioAdmin from "../components/InicioAdmin.vue"
-=======
 import Opciones from "../components/Opciones.vue"
-import instructores from "../components/Usuarios.vue"
->>>>>>> b1aea0ffb59a9b3dafd25799df988f49664b76f7
+import InicioAdmin from "../components/InicioAdmin.vue"
+import instructores from "../components/instructores.vue"
 
 export const routes = [
     { path: "/", component: Login },
     {
-<<<<<<< HEAD
         path: "/header", component: Header, children: [
             { path: "/inicioAdmin", component: InicioAdmin },
-=======
-        path: "/header",
-        component: Header,
-        redirect: "/header/opciones", // Redirige "/header" a "/header/opciones"
-        children: [
-            { path : "opciones" ,  component : Opciones },
-            { path : "instructores" , component : instructores },
->>>>>>> b1aea0ffb59a9b3dafd25799df988f49664b76f7
+            { path: "/opciones", component: Opciones },
+            { path: "/Instructores", component: instructores },
         ]
     },
 ]
+
+
