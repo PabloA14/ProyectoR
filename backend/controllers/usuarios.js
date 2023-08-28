7d0bb1e3d2828d10d714bfe9f3b57452dbb4d5ed
JuDaF-15 <juandda1505@gmail.com>
2023-08-28 16:40:50 -0500

    modificado modelo, ruta y peticiones de usuario

--- backend/controllers/usuarios.js
+++ backend/controllers/usuarios.js
@@ -1,13 +1,17 @@
 import Usuario from "../models/usuarios.js";
+import bcrypt from "bcrypt"
+
 //USUARIOS 
 const httpUsuario = {
 
     posUsuario: async (req, res) => {
-        const { cedula, nombre, apellidos, telefono, clave, correo, fechaNacimiento, redConocimiento, hojaDeVida, perfilProfesional, estado } = req.body;
+        const { cedula, nombre, apellidos, telefono, clave, correo, redConocimiento, hojaDeVida, rol, perfilProfesional, estado } = req.body;
 
         try {
+            const hashedPassword = await bcrypt.hash(clave, 10); // Hash the password
+
             const usuario = new Usuario({
-                cedula, nombre, apellidos, telefono, clave, correo, fechaNacimiento, redConocimiento, hojaDeVida, perfilProfesional, estado
+                cedula, nombre, apellidos, telefono, clave: hashedPassword, correo, redConocimiento, hojaDeVida, rol, perfilProfesional, estado
             });
             const buscar = await Usuario.findOne({ cedula: cedula });
             if (buscar) {
@@ -42,20 +46,51 @@ const httpUsuario = {
     },
 
     putUsuario: async (req, res) => {
-        const { cedula, nombre, apellidos, telefono, experiencia, redConocimiento, perfilProfesional } = req.body;
+        const usuarioId = req.params.id;
+
+        const {
+            cedula,
+            nombre,
+            apellidos,
+            telefono,
+            clave,
+            correo,
+            redConocimiento,
+            hojaDeVida,
+            rol,
+            perfilProfesional,
+            estado,
+        } = req.body;
 
         try {
+            // Check if the new cedula is already registered
+            const existingUser = await Usuario.findOne({ cedula: cedula });
+            if (existingUser && existingUser._id.toString() !== usuarioId) {
+                return res.status(400).json({ msg: 'La cedula ya está registrada para otro usuario' });
+            }
+
+            const updatedFields = {
+                nombre,
+                apellidos,
+                telefono,
+                correo,
+                redConocimiento,
+                hojaDeVida,
+                perfilProfesional,
+                rol,
+                estado,
+            };
+
+            if (clave) {
+                // If a new password is provided, hash it
+                const hashedPassword = await bcrypt.hash(clave, 10);
+                updatedFields.clave = hashedPassword;
+            }
+
             const updatedUsuario = await Usuario.findOneAndUpdate(
-                { cedula: cedula },
+                { _id: usuarioId },
                 {
-                    $set: {
-                        nombre,
-                        apellidos,
-                        telefono,
-                        experiencia,
-                        redConocimiento,
-                        perfilProfesional
-                    }
+                    $set: updatedFields,
                 },
                 { new: true }
             );
@@ -63,12 +98,13 @@ const httpUsuario = {
             if (!updatedUsuario) {
                 return res.status(404).json({ msg: 'Usuario no encontrado' });
             }
+
             res.status(200).json({ msg: 'Usuario actualizado exitosamente', usuario: updatedUsuario });
         } catch (error) {
             console.error(error);
             res.status(500).json({ msg: 'Error en el servidor' });
         }
-    }
+    },
 
 };
 
--- backend/routes/ususarios.js
+++ backend/routes/ususarios.js
@@ -6,7 +6,7 @@ const router = Router()
 router.post("/", httpUsuario.posUsuario)
 router.get("/traer", httpUsuario.getUsusarios)
 router.get("/todos", httpUsuario.getUsusarios)
-router.put("/edit", httpUsuario.putUsuario)
+router.put("/:id", httpUsuario.putUsuario)
 
 
 export default router
