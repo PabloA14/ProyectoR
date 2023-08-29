6f8469099bea8c6a6c60167a9793aceecfd45569
JuDaF-15 <juandda1505@gmail.com>
2023-08-29 12:43:00 -0500

    mod rutas

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -8,7 +8,7 @@ import Programas from "../components/ProgramasFormacion.vue"
 export const routes = [
     { path: "/", component: Login },
     {
-        path: "/header", component: Header, children: [
+        component: Header, children: [
             { path: "/opciones", component: Opciones },
             { path: "/usuarios", component: Usuarios },
             { path: "/Instructores", component: instructorInvitado },
