a967026a273ef248528bf1f73be9b53e12e74238
veronicvega <ardilavegaveronica@gmail.com>
2023-10-05 22:55:14 -0500

    prueba de rutas y gestor

---
+++ frontend/src/components/DesarrolloCurricular.vue
@@ -0,0 +1,72 @@
+<template>
+    <q-page padding>
+        <div class="text-h4 text-center q-mb-md">Desarrollo Curricular</div>
+     <div>
+        <div class="MatrizCorrelacion" >
+        <div class="row" id="row" >
+                <q-toolbar-title class="title" style="border-bottom: solid 1px; font-weight: 800;">
+                  Matriz de Correlación
+                </q-toolbar-title>
+          
+                <q-btn flat round dense>
+                  <q-icon name="more_vert" />
+                </q-btn>
+              </div>
+              <small>
+                Ver matriz de Correlación
+              </small>
+
+        </div>
+
+        <div class="MatrizCorrelacion" >
+        <div class="row" id="row" >
+                <q-toolbar-title style="border-bottom: solid 1px; font-weight: 800;">
+                    Proyecto Formativo 
+                </q-toolbar-title>
+          
+                <q-btn flat round dense>
+                  <q-icon name="more_vert" />
+                </q-btn>
+              </div>
+              <small>
+                Ver matriz de Proyecto Formativo
+              </small>
+
+        </div> 
+ 
+        <div class="MatrizCorrelacion"  >
+        <div class="row"  id="row">
+                <q-toolbar-title style="border-bottom: solid 1px; font-weight: 800;">
+                    Planeación Pedagógica
+                </q-toolbar-title>
+          
+                <q-btn flat round dense>
+                  <q-icon name="more_vert" />
+                </q-btn>
+              </div>
+              <small>
+                Ver matriz Planeación Pedagógica fffff
+              </small>
+
+        </div>
+
+    </div>
+
+    <div class="text-h4 text-center q-mb-md">Guías de Aprendizaje</div>
+
+
+
+</q-page>
+</template>
+
+<style scoped>
+
+.MatrizCorrelacion{
+    padding: 2%;
+    background-color: white;
+}
+
+
+
+
+</style>
