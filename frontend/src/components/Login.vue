315fe56937bab69e95858048d94ac77dc78c5c45
JuDaF-15 <juandda1505@gmail.com>
2023-10-07 11:39:50 -0500

    prueba de responsividad login en celular

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,117 +1,94 @@
 <template>
-      <q-layout view="hHh lpR fFf">
-        <q-page-container>
-            <q-page>
-
-          <div class="container">
-            <div class="izquierda"></div>
-            <div class="div" style="width: 2%;     background-color: #f0f0f0;
-">
-                
-            </div>
-            <div class="derecha">
-              <q-card white bordered class="my-card">
+    <div class="container">
+        <div class="izquierda"></div>
+        <div class="derecha">
+            <q-card white bordered class="my-card" style="width: 65%;height: auto;">
                 <q-card-section>
-                  <div class="q-gutter-md row justify-center">
-                    <div class="col-12 col-sm-3 text-center">
-                      <img id="img" src='../imagenes/logosena.png' alt="Logo" style="max-width: 100%;" />
+                    <div class="row">
+                        <div class="col-3">
+                            <img id="img" src='../imagenes/logosena.png'>
+                        </div>
+                        <div class="col-9">
+                            <div id="titulo" class="text-h6" style="text-align: center;">Iniciar Sesión</div>
+                        </div>
                     </div>
-                    <div class="col"></div>
-                    <div class="col-12 col-sm-9 text-center">
-                        <div class="text-h6 text-center" style="display: flex; justify-content: center; align-items: center;">Iniciar Sesión
-</div>
-                    </div>
-                  </div>
                 </q-card-section>
-                <q-separator inset color="black" />
+                <q-separator inset class="text-black" />
                 <q-card-section>
-                  <q-form class="q-gutter-md">
-                    <q-input color="secondary" filled v-model="documento" label="No. de Documento">
-                      <template v-slot:prepend>
-                        <q-icon name="person" />
-                      </template>
-                    </q-input>
-                    <q-input
-                      color="secondary"
-                      filled
-                      :type="mostrarContrasena ? 'text' : 'password'"
-                      v-model="contrasena"
-                      label="Contraseña"
-                    >
-                      <template v-slot:prepend>
-                        <q-icon name="lock" />
-                      </template>
-                      <template v-slot:append>
-                        <q-icon
-                          v-if="mostrarContrasena"
-                          name="remove_red_eye"
-                          @click="mostrarContrasena = !mostrarContrasena"
-                          style="cursor: pointer;"
-                        />
-                        <q-icon
-                          v-else
-                          name="remove_red_eye_off"
-                          @click="mostrarContrasena = !mostrarContrasena"
-                          style="cursor: pointer;"
-                        />
-                      </template>
-                    </q-input>
-                    <q-btn
-                      color="secondary"
-                      class="full-width q-mt-md"
-                      label="Ingresar"
-                      @click.prevent="iniciarSesion()"
-                    />
-                    <q-btn
-                      color="primary"
-                      class="full-width q-mt-sm"
-                      flat
-                      label="¿Olvidó su contraseña?"
-                      @click="openModal"
-                    />
-                    <q-spinner
-                      style="margin: 0 auto;"
-                      color="black"
-                      size="2em"
-                      :thickness="10"
-                      v-if="useUsuario.loading"
-                    />
-                  </q-form>
+                    <q-form class="q-gutter-md" style="margin: auto;">
+                        <q-input color="secondary" filled v-model="documento" label="No. de Documento">
+                            <template v-slot:prepend>
+                                <q-icon name="person" />
+                            </template>
+                        </q-input>
+
+                        <q-input color="secondary" filled :type="mostrarContrasena ? 'text' : 'password'"
+                            v-model="contrasena" label="Contraseña">
+                            <template v-slot:prepend>
+                                <q-icon name="lock" />
+                            </template>
+                            <template v-slot:append>
+                                <i @click="mostrarContrasena = !mostrarContrasena" style="cursor: pointer;" class="fa-solid"
+                                    :class="mostrarContrasena ? 'fa-eye' : 'fa-eye-slash'"></i>
+                            </template>
+
+
+                        </q-input><br>
+                        <!-- <p class="q-mt-sm">¿olvido su contraseña?</p> -->
+                        <p @click="openModal" class="q-mt-sm" style="cursor: pointer; color: rgb(5, 13, 255);text-align: center;">
+                            ¿OLVIDÓ
+                            SU
+                            CONTRASEÑA?</p><br>
+
+                        <div class="row">
+                            <q-spinner style="margin: 0 auto;" color="black" size="2em" :thickness="10"
+                                v-if="useUsuario.loading === true" />
+                            <q-btn v-else color="secondary" class="full-width" label="Ingresar"
+                                @click.prevent="iniciarSesion()" />
+                        </div><br>
+                    </q-form>
                 </q-card-section>
-              </q-card>
-            </div>
-            <q-dialog v-model="modalVisible" persistent>
-  <q-card style="min-width: 350px">
-    <q-card-section>
-      <div class="text-h6">Restablecer Contraseña</div>
-      <q-separator style="height: 5px; margin-top: 5px;" color="secondary" />
-    </q-card-section>
-
-    <q-card-section class="q-pt-none">
-      <p>Ingrese su dirección de correo electrónico para restablecer su contraseña:</p>
-      <q-input color="secondary" dense v-model="address" autofocus @keyup.enter="prompt = false" placeholder="Correo electrónico" />
-    </q-card-section>
-
-    <q-card-actions class="flex-center column">
-      <div class="row">
-        <div class="col-1"></div>
-        <div class="col-10">
-          <q-btn class="full-width" color="secondary" label="Restablecer Contraseña" />
-          <q-btn class="q-mt-md full-width custom-border" color="negative" label="Cancelar" v-close-popup />
+            </q-card>
         </div>
-        <div class="col-1"></div>
-      </div>
-    </q-card-actions>
-    <br>
-  </q-card>
-</q-dialog>
 
-          </div>
-        </q-page>
+        <q-dialog v-model="modalVisible" persistent>
+            <q-card style="min-width: 350px">
+                <q-card-section>
+                    <div class="text-h6">Restablecer Contraseña</div>
+                    <q-separator style="
+            height: 5px;
+            margin-top: 5px;
+          " color="secondary" />
+                </q-card-section>
 
-        </q-page-container>
-      </q-layout>
-  </template>
+                <q-card-section class="q-pt-none">
+                    <p>Ingrese su dirección de correo electrónico para
+                        <br>
+                        restablecer su contraseña:
+                    </p>
+                    <q-input color="secondary" dense v-model="address" autofocus @keyup.enter="prompt = false"
+                        placeholder="Correo electrónico" />
+                </q-card-section>
+
+                <q-card-actions class="flex-center column ">
+                    <div class="row">
+                        <div class="col-1"></div>
+                        <div class="col-10">
+
+                            <q-btn class="full-width" color="secondary" label="Restablecer Contraseña" />
+
+                            <q-btn class="q-mt-md full-width custom-border" color="negative" label="Cancelar"
+                                v-close-popup />
+                        </div>
+                        <div class="col-1"></div>
+                    </div>
+                </q-card-actions>
+                <br>
+            </q-card>
+        </q-dialog>
+    </div>
+</template>
+
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from "vue-router"
@@ -203,15 +180,6 @@ async function iniciarSesion() {
     background-image: url('../imagenes/login.jpg');
     background-size: cover;
     background-position: center;
-    
-}
-
-@media (max-width: 767px) {
-  .izquierda {
-    background-repeat: no-repeat;
-    background-size: cover;
-    height: 100%;
-  }
 }
 
 .derecha {
@@ -239,4 +207,12 @@ async function iniciarSesion() {
     border: 1px solid red;
 }
 
+@media screen and (max-width: 600px) {
+    .container {
+        flex-direction: column;
+    }
+    .izquierda {
+        display: none;
+    }
+}
 </style>
