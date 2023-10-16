17f52821eacdf1a98cfb1079807317acfb53bc6f
JuDaF-15 <juandda1505@gmail.com>
2023-10-16 09:43:14 -0500

    mod

--- frontend/src/components/GuiasAprendizaje.vue
+++ frontend/src/components/GuiasAprendizaje.vue
@@ -2,82 +2,47 @@
   <q-page padding>
     <div class="text-h4 text-center q-mb-md">Guías de Aprendizaje</div>
     <div class="q-pa-md" style="width: 100%">
-      <q-table
-        class="my-sticky-header-table"
-        :separator="separator"
-        bordered
-        :filter="filter"
-        :rows="rows"
-        :columns="columns"
-        row-key="name"
-        :pagination="pagination"
-      >
+      <q-table class="my-sticky-header-table" :separator="separator" bordered :filter="filter" :rows="rows"
+        :columns="columns" row-key="name" :pagination="pagination">
         <template v-slot:body-cell-opciones="props">
-          <q-td :props="props" >
+          <q-td :props="props">
             <div class="opciones">
 
               <router-link to="infoDesarrollo">
                 <!-- <q-icon title="Detalle de Programa" name="fa-solid fa-eye" color="primary" size="20px"
                   style="margin-right: 25px;cursor: pointer;" /> -->
-                  <q-btn color="secondary">
-                    <q-icon class="material-symbols-outlined" id="opciones" >
-                              visibility
-                    </q-icon>
-                  </q-btn>
+                <q-icon title="Detalle de Guía" name="fa-solid fa-eye" color="primary" size="25px"
+                  style="margin-right: 25px;cursor: pointer;" />
+              </router-link>
 
+              <router-link to="cards">
+                <q-icon title="Editar Guía" color="orange" name="fa-solid fa-pen-to-square fa-xl" size="25px"
+                  style="margin-right: 10px;cursor: pointer;" />
               </router-link>
 
               <router-link to="cards">
                 <!-- <q-icon title="Detalle de Programa" name="fa-solid fa-eye" color="primary" size="20px"
                   style="margin-right: 25px;cursor: pointer;" /> -->
-                  <q-btn color="secondary">
-                    <q-icon class="material-symbols-outlined" id="opciones">
-                              border_color
-                    </q-icon>
-                  </q-btn>
-
+                <q-icon title="Descargar guía" color="green" name="fa-solid fa-download" size="25px"
+                  style="margin-left: 10px;cursor: pointer;" />
               </router-link>
 
-
-              <router-link to="cards">
-                <!-- <q-icon title="Detalle de Programa" name="fa-solid fa-eye" color="primary" size="20px"
-                  style="margin-right: 25px;cursor: pointer;" /> -->
-                  <q-btn color="secondary">
-                    <q-icon class="material-symbols-outlined" style="" id="opciones">
-                              download
-                    </q-icon>
-                  </q-btn>
-
-              </router-link>
-
-              </div>
+            </div>
           </q-td>
         </template>
 
         <template v-slot:top-right>
-          <q-input
-            color="secondary"
-            dense
-            debounce="300"
-            v-model="filter"
-            placeholder="Buscar"
-          >
+          <q-input color="secondary" dense debounce="300" v-model="filter" placeholder="Buscar">
             <template v-slot:append>
               <q-icon name="search" />
             </template>
           </q-input>
         </template>
         <template v-slot:top-left>
-          <q-btn
-            color="secondary"
-            icon="add"
-            label="Agregar"
-            class="q-mb-md"
-            @click="
-              agregar = true;
-              nuevo();
-            "
-          />
+          <q-btn color="secondary" icon="add" label="Agregar" class="q-mb-md" @click="
+            agregar = true;
+          nuevo();
+          " />
         </template>
       </q-table>
     </div>
@@ -87,20 +52,18 @@
   
   <!-- @click="editarRol(props.row)" -->
 <style>
- #opciones{
-    font-size:4vh;
-  }
+#opciones {
+  font-size: 4vh;
+}
 
-  .opciones button {
-    margin-right: 8%;
-    color: white;
-  }
+.opciones button {
+  margin-right: 8%;
+  color: white;
+}
 </style>
 
 
-
-
-  <script setup>
+<script setup>
 const columns = [
   {
     name: "nombre",
@@ -109,7 +72,7 @@ const columns = [
     field: (row) => row.name,
     align: 'center'
   },
-  { name: "opciones", label: "Opciones", field: "opciones", sortable: false, align:'center' },
+  { name: "opciones", label: "Opciones", field: "opciones", sortable: false, align: 'center' },
 ];
 
 const rows = [
@@ -117,5 +80,6 @@ const rows = [
     name: "Guia de aprendizaje",
   },
 ];
+
 </script>
  
