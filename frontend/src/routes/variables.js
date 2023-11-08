73ad784fe36a29b3a81742ce138f4913667cc041
PabloA14 <138160222+PabloA14@users.noreply.github.com>
2023-11-08 16:55:52 -0500

    crud de guias

--- frontend/src/routes/variables.js
+++ frontend/src/routes/variables.js
@@ -1,4 +1,4 @@
 //no borrar ninguna de estas dos lineas , solo comentar y descomentar cuando necesiten
-export const LinkBD = "https://repositorio-mizv.onrender.com"
-//export const LinkBD = "http://localhost:4509"
+//export const LinkBD = "https://repositorio-mizv.onrender.com"
+export const LinkBD = "http://localhost:4509"
 //prueba
