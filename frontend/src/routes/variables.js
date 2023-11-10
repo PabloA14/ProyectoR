582d3dd477bb46950b65f4f695f9e86ad4b2df40
DavidMaluendas <vidjose017@gmail.com>
2023-11-10 17:39:07 -0500

    23342333

--- frontend/src/routes/variables.js
+++ frontend/src/routes/variables.js
@@ -1,6 +1,6 @@
 //no borrar ninguna de estas dos lineas , solo comentar y descomentar cuando necesiten
 //export const LinkBD = "https://repositorio-mizv.onrender.com"
-export const LinkBD = "http://localhost:4509"
+// export const LinkBD = "http://localhost:4509"
 
 //prueba
-// export const LinkBD = "https://repositorio-mizv.onrender.com"
+export const LinkBD = "https://repositorio-mizv.onrender.com"
