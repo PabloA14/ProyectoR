b64cb10bb338ca167519ff3fafe17148f43a738d
JuDaF-15 <119986261+JuDaF-15@users.noreply.github.com>
2023-11-14 00:02:04 -0500

    Update desarrolloCurricular.js

--- backend/routes/desarrolloCurricular.js
+++ backend/routes/desarrolloCurricular.js
@@ -33,7 +33,7 @@ routes.put("/proyectoFormativo/updated/D/:id", [
 routes.put("/planecion/updated/Pedagogica/D/:id", [
     check("planeacionPedagogica").custom((value, { req }) => {
         if (!req.files || Object.keys(req.files).length === 0) {
-            throw new Error('Debe adjuntar el Hatsune Miku');
+            throw new Error('Debe adjuntar el file---');
         }
         return true;
     }),
